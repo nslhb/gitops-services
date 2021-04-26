475bf2cc2820e475fd12438c8109ded0fb195136
Rohit Verma <80032502+rverma-nsl@users.noreply.github.com>
2021-04-26 14:34:16 +0530

    change paas source

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -16,24 +16,17 @@
 
 set -o errexit
 
-# mirror kustomize-controller build options
-kustomize_flags="--enable_kyaml=false --allow_id_changes=false --load_restrictor=LoadRestrictionsNone"
-kustomize_config="kustomization.yaml"
-
 find . -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
   do
-    if [[ "$file" == *"/templates/"* ]]; then
-      echo "INFO - Skip $file"
-    else
-      echo "INFO - Validating $file"
-      yq validate -d'*' "$file"
-    fi
+    echo "INFO - Validating $file"
+    yq e 'true' "$file" > /dev/null
 done
 
-find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
+k="kustomization.yaml"
+find . -type f -name $k -print0 | while IFS= read -r -d $'\0' file;
   do
-    echo "INFO - Validating kustomization ${file/%$kustomize_config}"
-    kustomize build "${file/%$kustomize_config}" $kustomize_flags | kubeval --ignore-missing-schemas
+    echo "INFO - Validating kustomization ${file/%$k}"
+    kustomize build "${file/%$k}" | kubeval --ignore-missing-schemas
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
     fi
